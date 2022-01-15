fafea6030698a039e6b3f3cced7df0b7f0fa9a2c
Étienne <elescarbeault@gmail.com>
2022-01-15 14:06:41 -0500

    formatted address extraction

--- src/main.py
+++ src/main.py
@@ -15,7 +15,13 @@ with open(fileName, 'r', encoding="cp866") as f:
 parsed_data = parseData(data)
 
 
-all_lat_long = getAllLatLong(parsed_data)
-locations = geocodio_client.reverse(all_lat_long)
+#all_lat_long = getAllLatLong(parsed_data)
+#locations = geocodio_client.reverse(all_lat_long)
+with open("../data/EMULATED_RESP.json", 'r') as f:
+    locations = json.load(f)
 
-print(locations)
+res_arr = locations["results"]
+for i, res_elem in enumerate(res_arr):
+    address_elem = res_elem["response"]["results"][0] # Most likely address is the first in the list
+    formatted_address = address_elem["formatted_address"]
+    print(formatted_address)
